626da4b407b8e48a52bf8c453edf34f4210684c0
Medhat84 <med_mahanni@yahoo.com>
2017-07-20 15:00:06 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+## Reading all datasets
 xtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
 ytrain <- read.table("UCI HAR Dataset/train/y_train.txt")
 xtest <- read.table("UCI HAR Dataset/test/X_test.txt")
@@ -7,16 +8,20 @@ activities <- read.table("UCI HAR Dataset/activity_labels.txt")
 subjecttrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
 subjecttest <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
+## Creating one dataset 
 train <- cbind(ytrain, subjecttrain, xtrain)
 test <- cbind(ytest, subjecttest, xtest)
 db <- rbind(train, test)
 
+## Giving names to activities & variables
 names(db) <- c("activity", "subject", as.character(features[,2]))
 db[,1] <- factor(db[,1], levels = activities[,1], labels = activities[,2])
 
+## Getting only mean & standard deviation measurements & correcting names of some columns
 meanstd <- db[ , c(1, 2, which(grepl("mean\\(|std\\(", names(db))))]
 names(meanstd) <- sub("BodyBody","Body",names(meanstd))
 
+## Grouping by activity then subject, getting mean & exporting the result tidy dataset
 library(dplyr)
 dset <- tbl_df(meanstd)
 avdset <- dset %>% group_by(activity, subject) %>% summarize_all(mean)
